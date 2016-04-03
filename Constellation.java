82cfdd3fdbec179124f280194f7af5a2eaf2a030
cgolner <cgolner@princeton.edu>
2016-04-02 22:58:56 -0400

    Added files via upload

---
+++ Constellation.java
@@ -0,0 +1,9 @@
+public class Constellation {
+        public double[] starAngles;
+        public String name;
+        
+        public Constellation(double[] starAngles, String name) {
+            this.starAngles = starAngles;
+            this.name = name;
+        }
+    }
